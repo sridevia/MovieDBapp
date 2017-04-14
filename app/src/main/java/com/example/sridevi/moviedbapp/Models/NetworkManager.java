63874ceb9008573bf91cbe78c91db4231a6d4a77
Sridevi <sridevi9akula@gmail.com>
2017-04-13 22:15:45 -0700

    Initial commit:
    Still need to work

---
+++ app/src/main/java/com/example/sridevi/moviedbapp/Models/NetworkManager.java
@@ -0,0 +1,42 @@
+package com.example.sridevi.moviedbapp.Models;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Sridevi on 4/13/2017.
+ */
+public class NetworkManager {
+    //https://api.themoviedb.org/3/movie/550?api_key=f85681f445e65e980c1fe51baa1cca05&search=avatar
+    public final String MOVIES_DB_URL = "https://api.themoviedb.org/3/movie/550";
+    public final String API_KEY = "f85681f445e65e980c1fe51baa1cca05";
+    public static NetworkManager INSTANCE = null;
+    private OkHttpClient okHttpClient;
+    private NetworkManager(){
+        okHttpClient = new OkHttpClient();
+    }
+    public static NetworkManager getInstance() {
+        if (INSTANCE == null)
+            return INSTANCE = new NetworkManager();
+        else
+            return INSTANCE;
+    }
+
+    public Request createSearchRequest(String searchText) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(MOVIES_DB_URL).newBuilder();
+        urlBuilder.addQueryParameter("api_key", API_KEY);
+        urlBuilder.addQueryParameter("search", searchText);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+        return  request;
+    }
+
+    public OkHttpClient getOkHttpClient() {
+        return this.okHttpClient;
+    }
+
+
+ }
---
+++ app/src/main/java/com/example/sridevi/moviedbapp/SearchMovieFragment.java
@@ -0,0 +1,133 @@
+package com.example.sridevi.moviedbapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sridevi.moviedbapp.Models.NetworkManager;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SearchMovieFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SearchMovieFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SearchMovieFragment extends Fragment {
+
+    public static final String TAG = "SearchMovieFragment";
+    private OnFragmentInteractionListener mListener;
+
+    private TextView mSearchTextView;
+    private EditText mEditText;
+
+    public SearchMovieFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SearchMovieFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SearchMovieFragment newInstance() {
+        SearchMovieFragment fragment = new SearchMovieFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_search_movie, container, false);
+        mSearchTextView = (TextView) rootView.findViewById(R.id.search_textview);
+        mEditText = (EditText)rootView.findViewById(R.id.enetr_movie);
+        Button mSearchButton = (Button) rootView.findViewById(R.id.submit_button);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchMovie();
+            }
+        });
+        return rootView;
+    }
+
+    private void searchMovie() {
+           String searchText = mSearchTextView.getText().toString();
+        Request movieSearchRequest = NetworkManager.getInstance().createSearchRequest(searchText);
+        NetworkManager.getInstance().getOkHttpClient().newCall(movieSearchRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG, e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, response.body().toString());
+            }
+        });
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
